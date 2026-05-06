2d3ba2991683fc9e064ff649638e4ab9f6c2c5d1
lurongshuang <lurongshuang@example.com>
2026-05-06 14:14:19 +0800

    版本号更新

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
 import sqflite_darwin
 import yuni_widget
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   YuniWidgetPlugin.register(with: registry.registrar(forPlugin: "YuniWidgetPlugin"))
 }
